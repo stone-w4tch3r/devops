f7b624b158b386c1e7007dc8f6d53039fa067af9
stone-w4thc3r <no@e.mail>
2023-04-24 00:59:55 +0500

    continue setup script

---
+++ scripts/puppet/continue_setup_puppet_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo ">>>this script continues puppet server setup"
+echo ">>>it should be run within puppet server system"
+echo ">>>it should be run after connecting pupput agent to server"
+echo ">>>dependencies: puppetserver, puppet-agent"
+
+#check input
+if [ $# -ne 3 ]; then
+    echo ">>>usage: continue_setup_puppet_server.sh <puppet_agent_hostname> <puppet_agent_username> <puppet_agent_password>"
+    exit 1
+fi
+
+puppet_agent_hostname=$1
+puppet_agent_username=$2
+puppet_agent_password=$3
+
+echo ">>>setting up ca-s (certificate authority)"
+sudo /opt/puppetlabs/bin/puppetserver ca setup
+
+echo ">>>certs in /etc/puppetlabs/puppet/ssl/certs/"
+ls /etc/puppetlabs/puppet/ssl/certs/
+
+echo ">>>signing certs"
+sudo /opt/puppetlabs/bin/puppetserver ca sign --certname "$puppet_agent_hostname"
