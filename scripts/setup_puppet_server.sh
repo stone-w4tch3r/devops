d523ae96ae85b4b9aff215b2ad3a4fbb0d9b6f7f
stone-w4thc3r <no@e.mail>
2023-04-23 18:01:05 +0500

    puppet scripts edited

--- scripts/setup_puppet_server.sh
+++ scripts/setup_puppet_server.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo ">>>this script sets up puppet agent on the system"
+echo ">>>this script sets up puppet server on the system"
 echo ">>>should be run within target system"
 echo ">>>installs puppet7"
 echo ">>>dependencies: ubuntu (debian?), wget, amd64 OR aarch64+20.04"
 
-#get and install puppet repo (based on current system)
+#get and install puppet repo
 wget https://apt.puppetlabs.com/puppet7-release-"$(lsb_release -cs)".deb
 sudo dpkg -i puppet7-release-"$(lsb_release -cs)".deb
 
 sudo apt-get update
-sudo apt-get install puppet-agent -y
+sudo apt-get install puppetserver -y
 
 #install ntp (for time sync)
 sudo apt-get -y install ntp
 sudo service ntp restart
 
-#run puppet agent
-sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+#run puppet server
+sudo /opt/puppetlabs/bin/puppet resource service puppetserver ensure=running enable=true
