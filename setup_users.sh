2dbd396946904e652ce66f30a180608a50416a6c
stone-w4thc3r <no@e.mail>
2023-04-23 15:58:43 +0500

    setup users script

---
+++ setup_users.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#this script sets up users and groups for the system
+#it gets 3 arguments: new username, new password, and new root password
+#it changes root password, adds new user, and adds new user to sudo and docker group
+
+main(){
+    local username=$1
+    local password=$2
+    local root_password=$3
+    echo ">>>setting up users"
+    
+    echo ">>>changing root password"
+    echo "root:$root_password" | sudo chpasswd
+    
+    echo ">>>adding user"
+    #--gecos "" prevents user from being prompted for additional info (like full name)
+    sudo adduser --gecos "" "$username"
+    echo "$username:$password" | sudo chpasswd
+    
+    echo ">>>adding user to sudo and docker group"
+    #-aG means append to group
+    sudo usermod -aG sudo "$username"
+    sudo usermod -aG docker "$username"
+}
+
+if [ $# -ne 3 ]; then
+    echo "Usage: setup_users.sh username password root_password"
+    return 1
+fi
+
+main "$1" "$2" "$3"
