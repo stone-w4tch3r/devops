90972abc9788169daf5f551c15674d2b5752d215
stone-w4thc3r <no@e.mail>
2023-04-23 18:36:51 +0500

    ufw script

---
+++ scripts/setup_ufw.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ">>>this script sets up ufw firewall"
+
+echo ">>>enabling ufw"
+sudo ufw enable
+
+echo ">>>allowing ssh"
+sudo ufw allow ssh
+
+echo ">>>allowing puppet"
+sudo ufw allow 8140
