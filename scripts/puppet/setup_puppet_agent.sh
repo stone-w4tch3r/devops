0a8be0d3df9ccfeaae9c21e6d5e25ce9edba8e65
stone-w4thc3r <no@e.mail>
2023-05-02 16:02:09 +0500

    puppet6 -> puppet7

--- scripts/puppet/setup_puppet_agent.sh
+++ scripts/puppet/setup_puppet_agent.sh
@@ -2,7 +2,7 @@
 
 echo ">>>this script sets up puppet agent on the system"
 echo ">>>should be run within target system"
-echo ">>>installs puppet6"
+echo ">>>installs puppet7"
 echo ">>>dependencies: ubuntu (debian?), wget, amd64 OR aarch64+20.04"
 
 if [ -z "$1" ]; then
@@ -13,8 +13,8 @@ fi
 server_ip=$1
 
 #get and install puppet repo (based on current system)
-wget https://apt.puppetlabs.com/puppet6-release-"$(lsb_release -cs)".deb
-sudo dpkg -i puppet6-release-"$(lsb_release -cs)".deb
+wget https://apt.puppetlabs.com/puppet7-release-"$(lsb_release -cs)".deb
+sudo dpkg -i puppet7-release-"$(lsb_release -cs)".deb
 
 sudo apt-get update
 sudo apt-get install puppet-agent -y
