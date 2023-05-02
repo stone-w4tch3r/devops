60289e024855a5fc3f3b6cc65eaf7e36d0a1548c
stone-w4thc3r <no@e.mail>
2023-05-02 12:40:25 +0500

    continue?

--- scripts/multipass/setup_multipass.sh
+++ scripts/multipass/setup_multipass.sh
@@ -3,6 +3,11 @@
 echo ">>>this script sets up multipass instance"
 echo ">>>should be run within multipass instance"
 
+read -p ">>>continue? (y/n) " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
 echo ">>>setting up ubuntu user password"
 echo "ubuntu:ubuntu" | sudo chpasswd
 
