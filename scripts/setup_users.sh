151f8e758f80213aa97d070b87c39f1d61b3101f
stone-w4thc3r <no@e.mail>
2023-05-17 16:14:12 +0500

    setup_server.sh done

--- scripts/setup_users.sh
+++ scripts/setup_users.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-echo ">>>this script sets up users and groups for the system"
-
-main(){
-    local username=$1
-    local password=$2
-    local root_password=$3
-    echo ">>>setting up users"
-    
-    echo ">>>changing root password"
-    echo "root:$root_password" | sudo chpasswd
-    
-    echo ">>>adding user"
-    #--gecos "" prevents user from being prompted for additional info (like full name), --disabled-password prevents user from logging in with password
-    sudo useradd --gecos "" --disabled-password "$username"
-    echo "$username:$password" | sudo chpasswd
-    
-    echo ">>>adding user to sudo and docker group"
-    #-aG means append to group
-    sudo usermod -aG sudo "$username"
-    sudo usermod -aG docker "$username"
-}
+echo ">>>this script creates new sudo user and changes root password. should be run within target system"
 
 if [ $# -ne 3 ]; then
-    echo "Usage: setup_users.sh username password root_password"
-    return 1
+  echo "Usage: $0 new_username new_password new_root_password"
+  exit 1
 fi
 
-main "$1" "$2" "$3"
+new_username=$1
+new_password=$2
+new_root_password=$3
+echo ">>>setting up users and passwords"
+
+echo ">>>changing root password"
+echo "root:$new_root_password" | sudo chpasswd
+
+echo ">>>adding user"
+sudo useradd "$new_username"
+echo "$new_username:$new_password" | sudo chpasswd
+
+echo ">>>creating docker group"
+sudo groupadd docker
+
+echo ">>>adding user to sudo and docker group"
+#-aG means append to group
+sudo usermod -aG sudo "$new_username"
+sudo usermod -aG docker "$new_username"
