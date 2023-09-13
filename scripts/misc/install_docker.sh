c985fcc6b722a30b916fe8796bb430b8cbf73554
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-09-13 10:22:55 +0500

    Update install_docker.sh

--- scripts/misc/install_docker.sh
+++ scripts/misc/install_docker.sh
@@ -3,7 +3,7 @@
 echo ">>>this script installs docker. should be run within target system"
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -15,7 +15,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
