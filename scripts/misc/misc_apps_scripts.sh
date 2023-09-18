1e5e6be39840b0124556915c7ca11e1a0231eb61
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-09-18 21:08:27 +0500

    Create misc_apps_scripts.sh

---
+++ scripts/misc/misc_apps_scripts.sh
@@ -0,0 +1,12 @@
+return 0;
+
+#non-executable
+
+
+####
+#speedtest
+####
+
+sudo apt-get install -y curl
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+sudo apt-get install speedtest
