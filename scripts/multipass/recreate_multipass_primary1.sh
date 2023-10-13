b57bd4a4dd8ecdd849d9ef8e0e99bde56999ccef
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-13 23:55:45 +0500

    optimized and cleaned recreate_multipass_primary1.sh

--- scripts/multipass/recreate_multipass_primary1.sh
+++ scripts/multipass/recreate_multipass_primary1.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 sudo echo ">>>this is a script that recreates the multipass instance and sets /etc/hosts"
-multipass delete primary1 && multipass purge 
-multipass launch -d 15G -n primary1 --cloud-init vm-cloud-init.yml
-primary_ip="$(multipass list | grep primary | awk -F' +' '{print $3}')" && echo "$primary_ip" | sudo sed -i "s|.*primary.*|$primary_ip primary.multipass|g" /etc/hosts
+
+multipass stop primary1
+multipass delete primary1
+multipass purge 
+echo ">>>multipass instances stopped and deleted"
+
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+multipass launch -d 15G -n primary1 --cloud-init "$script_path"/vm-cloud-init.yml
+
+primary_ip="$(multipass list | grep primary | awk -F' +' '{print $3}')"
+sudo sed -i "s|.*primary.*|$primary_ip primary.multipass|g" /etc/hosts
