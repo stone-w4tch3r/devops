63828a3038281d5a6e47126dacd1289fef0aef16
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-18 16:23:42 +0500

    lint, choco experiments, ps1

--- scripts/windows-vm/setup_ssh.ps1
+++ scripts/windows-vm/setup_ssh.ps1
@@ -6,3 +6,4 @@ Set-Service -Name ssh-agent -StartupType 'Automatic'
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
     New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 }
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
