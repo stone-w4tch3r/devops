04321afb956a8e630eec515287623fd8e3a58aab
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-27 11:32:24 +0500

    updated multipass scripts and cloud-init

--- scripts/multipass/recreate_multipass_puppetserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ">>>this is a script that recreates the multipass instance"
-echo ">>>dependencies: multipass"
-echo ">>>instance specs: 15GB disk, 1GB ram, 1 cpu, ubuntu 20.04"
-
-multipass delete puppetserver
-multipass purge
-multipass launch --name puppetserver --disk 15G --memory 3G --cpus 2 focal
