c48956e0b90c96f8e218a318577d92335286abf7
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-10-31 22:05:08 +0500

    added sudo pass

--- pyinfra/inventory.py
+++ pyinfra/inventory.py
@@ -1,3 +1,3 @@
 my_hosts = [
-    ("primary.multipass", {"ssh_user": "ubuntu", "ssh_password": "ubuntu", "test_setting": "test_value"}),
+    ("primary.multipass", {"ssh_user": "ubuntu", "ssh_password": "ubuntu", "_use_sudo_password": "ubuntu", "test_setting": "test_value"}),
 ]
