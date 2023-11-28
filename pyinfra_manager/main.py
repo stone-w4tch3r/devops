9802b1fadf13136f8e7a8efcac07201c8f71fbe1
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-11-28 21:02:44 +0500

    main updated

--- pyinfra_manager/main.py
+++ pyinfra_manager/main.py
@@ -1,6 +1,9 @@
+from deploys.fali2ban import deploy_fail2ban
+from deploys.root_password import deploy_root_password
 from deploys.server_upgrade import server_upgrade
 from deploys.server_user import deploy_server_user
 from deploys.shell_aliases import deploy_aliases
+from deploys.ssh_hardening import deploy_ssh_hardening
 from deploys.ssh_keyed_connection import deploy_ssh_keyed_connection
 from deploys.ufw import deploy_ufw
 from deploys.zsh import deploy_zsh
@@ -9,8 +12,11 @@ from deploys.shell_tools import deploy_shell_tools
 
 server_upgrade()
 deploy_server_user()
-deploy_zsh()
 deploy_shell_tools()
+deploy_zsh()
 deploy_aliases()
 deploy_ssh_keyed_connection()
 deploy_ufw()
+deploy_root_password()
+deploy_ssh_hardening()
+# deploy_fail2ban()
