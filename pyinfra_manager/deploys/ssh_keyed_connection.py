13d24f1ea2cf4d53a1ad9a463a585f903bef6bb5
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-11-29 11:23:52 +0500

    improved idempotency, refactored

--- pyinfra_manager/deploys/ssh_keyed_connection.py
+++ pyinfra_manager/deploys/ssh_keyed_connection.py
@@ -21,13 +21,16 @@ def update_connection_data(ssh_key: str, server_user: str, server_user_password:
         host.data.override_datas["ssh_password"] = server_user_password
 
 
-def generate_and_install_ssh_key_locally(ssh_key: str, local_known_hosts: str) -> None:
+def update_known_hosts_locally(local_known_hosts: str) -> None:
     run_locally(f"ssh-keygen -f {local_known_hosts} -R primary.multipass", ignore_errs=True)
-    run_locally(f"ssh-keyscan -H {host.name} >> {local_known_hosts}")
+    run_locally(f"ssh-keyscan {host.name} >> {local_known_hosts}")
 
+
+def generate_keys(ssh_key: str) -> None:
     if os.path.isfile(ssh_key):
         os.remove(ssh_key)
         os.remove(ssh_key + ".pub")
+
     # -q: quiet, -N: passphrase, -f: filename
     run_locally(f"ssh-keygen -t rsa -f {ssh_key} -q -N ''", ignore_errs=True)
     run_locally(f"chmod 600 {ssh_key}")
@@ -40,17 +43,34 @@ def is_ssh_keyed_connection_deployed(local_known_hosts: str, ssh_key_path: str) 
 
 
 def deploy_ssh_keyed_connection() -> None:
-    local_known_hosts = f"/home/{os.getlogin()}/.ssh/known_hosts"
-    overwrite = host.data.get("overwrite_ssh") is True
-    ssh_key = host.data.ssh_key_path
+    ssh_key_path = host.data.ssh_key_path
     server_user = host.data.server_user
     server_user_password = host.data.server_user_password
+    overwrite = host.data.get("overwrite_ssh") is True
 
-    python.call(lambda: assert_user_exists(server_user))
+    local_known_hosts = f"/home/{os.getlogin()}/.ssh/known_hosts"
+    is_server_in_known_hosts = host.name in open(local_known_hosts).read()
+    if overwrite or not is_server_in_known_hosts:
+        python.call(
+            name="Add server to known_hosts",
+            function=lambda: update_known_hosts_locally(local_known_hosts)
+        )
 
-    if overwrite or not is_ssh_keyed_connection_deployed(local_known_hosts, ssh_key):
-        python.call(lambda: generate_and_install_ssh_key_locally(ssh_key, local_known_hosts))
-
-    python.call(lambda: server.user_authorized_keys(user=server_user, public_keys=[ssh_key + ".pub"], _sudo=True))
-
-    python.call(lambda: update_connection_data(ssh_key, server_user, server_user_password))
+    is_ssh_key_existing = os.path.isfile(ssh_key_path)
+    if overwrite or not is_ssh_key_existing:
+        python.call(
+            name="Assert user exists",
+            function=lambda: assert_user_exists(server_user)
+        )
+        python.call(
+            name="Generate keys",
+            function=lambda: generate_keys(ssh_key_path)
+        )
+        python.call(
+            name="Install public key on server",
+            function=lambda: server.user_authorized_keys(user=server_user, public_keys=[ssh_key_path + ".pub"], _sudo=True)
+        )
+        python.call(
+            name="Update connection data",
+            function=lambda: update_connection_data(ssh_key_path, server_user, server_user_password)
+        )
