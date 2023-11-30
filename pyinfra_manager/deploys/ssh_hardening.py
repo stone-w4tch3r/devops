d944aab911758bf773b366632789a28a30ac2857
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-11-30 19:15:03 +0500

    ssh_hardening bug fixed + idempotency improved

--- pyinfra_manager/deploys/ssh_hardening.py
+++ pyinfra_manager/deploys/ssh_hardening.py
@@ -1,4 +1,5 @@
 from pyinfra import host
+from pyinfra.api.operation import OperationMeta
 from pyinfra.operations import files, server
 
 from deploys.ssh_hardening_vars import ssh_hardening_rules
@@ -7,8 +8,12 @@ from deploys.ssh_hardening_vars import ssh_hardening_rules
 def deploy_ssh_hardening():
     configs = ["/etc/ssh/sshd_config", *host.get_fact(files.FindFiles, "/etc/ssh/sshd_config.d")]
 
+    results = []
     for config in configs:
         for rule in ssh_hardening_rules:
-            files.line(path=config, replace=rule.RegexToReplace, line=rule.RuleText, _sudo=True)
+            res = files.line(path=config, replace=rule.RuleText, line=rule.RegexToReplace, _sudo=True)
+            results.append(res)
 
-    server.service(service="sshd", restarted=True, _sudo=True)
+    were_changes = True in [result.changed for result in results]
+    if were_changes:
+        server.service(service="sshd", restarted=True, _sudo=True)
