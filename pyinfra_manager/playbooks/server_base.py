678f7653e5a31268f7e6204dd91a789a081f4e80
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-02 20:13:52 +0500

    server_base play

---
+++ pyinfra_manager/playbooks/server_base.py
@@ -0,0 +1,22 @@
+from pyinfra import host
+
+from deploys.disable_ipv6 import disable_ipv6
+from deploys.root_password import deploy_root_password
+from deploys.server_upgrade import server_upgrade
+from deploys.server_user import deploy_server_user
+from deploys.shell_aliases import deploy_aliases
+from deploys.ssh_hardening import deploy_ssh_hardening
+from deploys.ssh_keys import deploy_ssh_keys
+from deploys.ufw import deploy_ufw
+
+# ugly =(
+host.data.override_datas["instance_complexity"] = "Minimal"
+
+server_upgrade()
+deploy_server_user()
+deploy_aliases()
+deploy_ssh_keys()
+deploy_ssh_hardening()
+deploy_ufw()
+deploy_root_password()
+disable_ipv6()
