4aa6ee2677106ce187b657471786becc1c80a97d
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-02 19:09:33 +0500

    tests small fix

--- pyinfra_manager/scripts/simple_tests.py
+++ pyinfra_manager/scripts/simple_tests.py
@@ -9,22 +9,24 @@ first_distro = "jammy"
 second_distro = "focal"
 third_distro = "file:///home/user1/VMs/debian-12-generic-amd64.qcow2"
 
+first_run = "pyinfra inventory.py all_deploys.py --data instance_complexity=Normal"
+second_run = "pyinfra inventory.py all_deploys.py --data instance_complexity=Extended"
+
 print("++++++++++++++++++++++++++++++++")
 print("running simple tests")
 print("++++++++++++++++++++++++++++++++")
 
 print("first deploy")
 run(f"scripts/recreate_target.py {first_distro}")
-run("pyinfra inventory.py all_deploys.py --data instance_complexity=Basic")
-run("pyinfra inventory.py all_deploys.py --data instance_complexity=Extended")
+run(first_run)
+run(second_run)
 
 print("second deploy")
 run(f"scripts/recreate_target.py {second_distro}")
-run("pyinfra inventory.py all_deploys.py --data instance_complexity=Basic")
-run("pyinfra inventory.py all_deploys.py --data instance_complexity=Extended")
+run(first_run)
+run(second_run)
 
 print("third deploy")
 run(f"scripts/recreate_target.py {third_distro}")
-run("pyinfra inventory.py all_deploys.py --data instance_complexity=Basic")
-run("pyinfra inventory.py all_deploys.py --data instance_complexity=Extended")
-
+run(first_run)
+run(second_run)
