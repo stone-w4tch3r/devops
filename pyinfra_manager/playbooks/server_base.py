603e9ba8ed471064c0189c835ed8594b40a9f824
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-04 01:00:19 +0500

    added variables override

--- pyinfra_manager/playbooks/server_base.py
+++ pyinfra_manager/playbooks/server_base.py
@@ -9,8 +9,8 @@ from deploys.ssh_hardening import deploy_ssh_hardening
 from deploys.ssh_keys import deploy_ssh_keys
 from deploys.ufw import deploy_ufw
 
-if not host.data.aliases_complexity:
-    host.data.aliases_complexity = "Minimal"
+# todo remove, too ugly
+host.data.aliases_complexity = "Minimal"
 
 server_upgrade()
 deploy_server_user()
