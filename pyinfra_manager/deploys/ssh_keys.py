9a758b0b04ace204aeed937cd15cda86813442f4
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-03 12:32:32 +0500

    ssh_keyed_connection -> ssh_keys + connection data update removed

---
+++ pyinfra_manager/deploys/ssh_keys.py
@@ -0,0 +1,63 @@
+import os
+import subprocess
+
+from pyinfra.operations import server, python
+from pyinfra import host
+
+
+def _run_locally(cmd: str, ignore_errs: bool = False) -> None:
+    subprocess.run(cmd, shell=True, check=not ignore_errs)
+
+
+def _assert_user_exists(user: str) -> None:
+    assert user in [user for user in host.reload_fact(server.Users)]
+
+
+def _update_known_hosts_locally(local_known_hosts: str) -> None:
+    _run_locally(f"ssh-keygen -f {local_known_hosts} -R primary.multipass", ignore_errs=True)
+    _run_locally(f"ssh-keyscan {host.name} >> {local_known_hosts}")
+
+
+def _generate_keys(ssh_key: str) -> None:
+    if os.path.isfile(ssh_key):
+        os.remove(ssh_key)
+        os.remove(ssh_key + ".pub")
+
+    # -q: quiet, -N: passphrase, -f: filename
+    _run_locally(f"ssh-keygen -t rsa -f {ssh_key} -q -N ''", ignore_errs=True)
+    _run_locally(f"chmod 600 {ssh_key}")
+
+
+def _is_ssh_keyed_connection_deployed(local_known_hosts: str, ssh_key_path: str) -> bool:
+    is_server_in_known_hosts = host.name in open(local_known_hosts).read()
+    is_ssh_key_exists = os.path.isfile(ssh_key_path)
+    return is_server_in_known_hosts and is_ssh_key_exists
+
+
+def deploy_ssh_keys() -> None:
+    ssh_key_path = host.data.ssh_key_path
+    server_user = host.data.server_user
+    overwrite = host.data.get("overwrite_ssh") is True
+
+    local_known_hosts = f"/home/{os.getlogin()}/.ssh/known_hosts"
+    is_server_in_known_hosts = host.name in open(local_known_hosts).read()
+    if overwrite or not is_server_in_known_hosts:
+        python.call(
+            name="Add server to known_hosts",
+            function=lambda: _update_known_hosts_locally(local_known_hosts)
+        )
+
+    is_ssh_key_existing = os.path.isfile(ssh_key_path)
+    if overwrite or not is_ssh_key_existing:
+        python.call(
+            name="Assert user exists",
+            function=lambda: _assert_user_exists(server_user)
+        )
+        python.call(
+            name="Generate keys",
+            function=lambda: _generate_keys(ssh_key_path)
+        )
+        python.call(
+            name="Install public key on server",
+            function=lambda: server.user_authorized_keys(user=server_user, public_keys=[ssh_key_path + ".pub"], _sudo=True)
+        )
--- pyinfra_manager/main.py
+++ pyinfra_manager/main.py
@@ -3,7 +3,7 @@ from deploys.server_upgrade import server_upgrade
 from deploys.server_user import deploy_server_user
 from deploys.shell_aliases import deploy_aliases
 from deploys.ssh_hardening import deploy_ssh_hardening
-from deploys.ssh_keyed_connection import deploy_ssh_keyed_connection
+from deploys.ssh_keys import deploy_ssh_keys
 from deploys.ufw import deploy_ufw
 from deploys.zsh import deploy_zsh
 from deploys.disable_ipv6 import disable_ipv6
@@ -14,7 +14,7 @@ deploy_server_user()
 deploy_shell_tools()
 deploy_zsh()
 deploy_aliases()
-deploy_ssh_keyed_connection()
+deploy_ssh_keys()
 deploy_ufw()
 deploy_ssh_hardening()
 deploy_root_password()
