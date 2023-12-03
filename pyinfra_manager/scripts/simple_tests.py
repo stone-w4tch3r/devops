36af412800ca813a8868b15c6e8c519ee45a751c
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-04 00:58:26 +0500

    refactoring + naming + root login fixed

--- pyinfra_manager/scripts/simple_tests.py
+++ pyinfra_manager/scripts/simple_tests.py
@@ -5,51 +5,54 @@ def run(cmd: str, ignore_errs: bool = False):
     return subprocess.run(cmd, shell=True, check=not ignore_errs)
 
 
-first_distro = "jammy"
-second_distro = "file:///home/user1/VMs/debian-12-generic-amd64.qcow2"
+ubuntu_distro = "jammy"
+debian_distro = "file:///home/user1/VMs/debian-12-generic-amd64.qcow2"
 
-first_run = ("pyinfra inventory.py playbooks/all.py "
-             + "--data aliases_complexity=Normal "
-             + "--data zsh_complexity=Normal "
-             + "--data tools_complexity=Normal")
-second_run = ("pyinfra inventory.py playbooks/all.py "
-              + "--data aliases_complexity=Extended "
-              + "--data zsh_complexity=Extended "
-              + "--data tools_complexity=Extended")
-third_run = ("pyinfra inventory.py playbooks/all.py "
-             + "--ssh-user root --ssh-password rnd_root_p4ss "
-             + "--data aliases_complexity=Normal "
-             + "--data zsh_complexity=Normal "
-             + "--data tools_complexity=Normal")
-fourth_run = ("pyinfra inventory.py playbooks/all.py "
-              + "--ssh-user root --ssh-password rnd_root_p4ss "
-              + "--data aliases_complexity=Extended "
-              + "--data zsh_complexity=Extended "
-              + "--data tools_complexity=Extended")
+run_basic_complexity_normal = ("pyinfra inventory.py playbooks/all.py "
+                               + "--data aliases_complexity=Normal "
+                               + "--data zsh_complexity=Normal "
+                               + "--data tools_complexity=Normal")
+run_basic_complexity_extended = ("pyinfra inventory.py playbooks/all.py "
+                                 + "--data aliases_complexity=Extended "
+                                 + "--data zsh_complexity=Extended "
+                                 + "--data tools_complexity=Extended")
+run_rooted_complexity_normal = ("pyinfra inventory.py playbooks/all.py "
+                                + "--ssh-user root --ssh-password rnd_root_p4ss "
+                                + "--data aliases_complexity=Normal "
+                                + "--data zsh_complexity=Normal "
+                                + "--data tools_complexity=Normal")
+run_rooted_complexity_extended = ("pyinfra inventory.py playbooks/all.py "
+                                  + "--ssh-user root --ssh-password rnd_root_p4ss "
+                                  + "--data aliases_complexity=Extended "
+                                  + "--data zsh_complexity=Extended "
+                                  + "--data tools_complexity=Extended")
+
+enable_root_command = """pyinfra inventory.py --sudo exec -- 'echo "root:rnd_root_p4ss" | sudo chpasswd'"""
 
 print("++++++++++++++++++++++++++++++++")
 print("running simple tests")
 print("++++++++++++++++++++++++++++++++")
 
-print("first deploy")
-run(f"scripts/recreate_target.py {first_distro}")
-run(first_run)
-run(second_run)
+print("deploy: basic, ubuntu")
+run(f"scripts/recreate_target.py {ubuntu_distro}")
+run(run_basic_complexity_normal)
+run(run_basic_complexity_extended)
 
-print("second deploy")
-run(f"scripts/recreate_target.py {second_distro}")
-run(first_run)
-run(second_run)
+print("deploy: basic, debian")
+run(f"scripts/recreate_target.py {debian_distro}")
+run(run_basic_complexity_normal)
+run(run_basic_complexity_extended)
 
-print("third deploy")
-run(f"scripts/recreate_target.py {first_distro}")
-run(third_run)
-run(fourth_run)
+print("deploy: rooted, ubuntu")
+run(f"scripts/recreate_target.py {ubuntu_distro}")
+run(run_rooted_complexity_normal)
+run(run_rooted_complexity_extended)
 
-print("fourth deploy")
-run(f"scripts/recreate_target.py {second_distro}")
-run(third_run)
-run(fourth_run)
+print("deploy: rooted, debian")
+run(enable_root_command)
+run(f"scripts/recreate_target.py {debian_distro}")
+run(run_rooted_complexity_normal)
+run(run_rooted_complexity_extended)
 
 print("++++++++++++++++++++++++++++++++")
 print("tests passed")
