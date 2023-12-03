32452184ed5cb8f4eca5d824ca9f2cacdc21f0fa
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-03 14:47:47 +0500

    added default aliases_complexity

--- pyinfra_manager/playbooks/server_base.py
+++ pyinfra_manager/playbooks/server_base.py
@@ -9,12 +9,14 @@ from deploys.ssh_hardening import deploy_ssh_hardening
 from deploys.ssh_keys import deploy_ssh_keys
 from deploys.ufw import deploy_ufw
 
+if not host.data.aliases_complexity:
+    host.data.aliases_complexity = "Minimal"
 
 server_upgrade()
 deploy_server_user()
-deploy_aliases()
-deploy_ssh_keys()
 deploy_ssh_hardening()
+deploy_ssh_keys()
+deploy_aliases()
 deploy_ufw()
 deploy_root_password()
 disable_ipv6()
