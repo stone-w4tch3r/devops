5192966a88f67fcc28663110390783717cbe4db1
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-03 12:31:26 +0500

    aliases dynamic bashrc/zshrc resolution added

--- pyinfra_manager/deploys/shell_aliases.py
+++ pyinfra_manager/deploys/shell_aliases.py
@@ -1,9 +1,21 @@
 from pyinfra import host
 from pyinfra.operations import files, python
 from pyinfra.facts import server as facts_server
-
 from deploys.shell_aliases_vars import aliases_vars
 from inventory_types import InstanceComplexity
+
+
+def _find_target_file_and_write_aliases(aliases_block_content: str, home_path: str) -> None:
+    target_file = f"{home_path}/.bashrc"
+    if host.reload_fact(files.File, f"{home_path}/.zshrc"):
+        target_file = f"{home_path}/.zshrc"
+
+    files.block(
+        path=target_file,
+        content=aliases_block_content,
+        marker="#### {mark} ALIASES BLOCK ####",
+        try_prevent_shell_expansion=True,
+    )
 
 
 def deploy_aliases() -> None:
@@ -14,15 +26,8 @@ def deploy_aliases() -> None:
         aliases_block_content += aliases_vars.AliasesNormal
     if instance_complexity == InstanceComplexity.Extended:
         aliases_block_content += aliases_vars.AliasesNormal + aliases_vars.AliasesExtended
-    target_file = f"{home_path}/.bashrc"
-    if host.reload_fact(files.File, f"{home_path}/.zshrc"):
-        target_file = f"{home_path}/.zshrc"
 
     python.call(
-        function=lambda: files.block(
-            path=target_file,
-            content=aliases_block_content,
-            marker="#### {mark} ALIASES BLOCK ####",
-            try_prevent_shell_expansion=True,
-        )
+        name="Find target file and write aliases",
+        function=lambda: _find_target_file_and_write_aliases(aliases_block_content, home_path)
     )
