ddb755f688048673dc7e301f26a210cdbf88f277
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-07 13:42:37 +0500

    added ufw_disable_ipv6

---
+++ pyinfra_manager/deploys/ufw_disable_ipv6.py
@@ -0,0 +1,23 @@
+from pyinfra import host
+from pyinfra.facts import deb
+from pyinfra.operations import files, server, python
+
+
+def _assert_ufw_installed():
+    assert host.reload_fact(deb.DebPackage, "ufw"), "ufw is not installed"
+
+
+def ufw_disable_ipv6():
+    python.call(
+        name="Assert ufw installed",
+        function=lambda: _assert_ufw_installed()
+    )
+
+    r = files.line(path="/etc/default/ufw", replace="IPV6=no", line="^IPV6=.*", _sudo=True)
+
+    if r.changed:
+        server.shell(
+            name="Shell: Restart ufw",
+            commands="sudo ufw reload",
+            _sudo=True,
+        )
--- pyinfra_manager/main.py
+++ pyinfra_manager/main.py
@@ -5,17 +5,19 @@ from deploys.shell_aliases import deploy_aliases
 from deploys.ssh_hardening import deploy_ssh_hardening
 from deploys.ssh_keys import deploy_ssh_keys
 from deploys.ufw import deploy_ufw
+from deploys.ufw_disable_ipv6 import ufw_disable_ipv6
 from deploys.zsh import deploy_zsh
 from deploys.disable_ipv6 import disable_ipv6
 from deploys.shell_tools import deploy_shell_tools
 
-server_upgrade()
-deploy_server_user()
-deploy_shell_tools()
-deploy_zsh()
-deploy_aliases()
-deploy_ssh_keys()
-deploy_ufw()
-deploy_ssh_hardening()
-deploy_root_password()
-disable_ipv6()
+# server_upgrade()
+# deploy_server_user()
+# deploy_shell_tools()
+# deploy_zsh()
+# deploy_aliases()
+# deploy_ssh_keys()
+# deploy_ufw()
+# deploy_ssh_hardening()
+# deploy_root_password()
+# disable_ipv6()
+ufw_disable_ipv6()
