b3d586c0c01ff03a6e59c092fd239a9ba50c23d7
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-12-17 13:18:54 +0500

    added xui folder

--- pyinfra_manager/main.py
+++ pyinfra_manager/main.py
@@ -5,14 +5,14 @@ from tasks.shell_aliases import deploy_aliases
 from tasks.ssh_hardening import deploy_ssh_hardening
 from tasks.ssh_keys import deploy_ssh_keys
 from tasks.ufw import deploy_ufw
-from tasks.ufw_disable_ipv6 import ufw_disable_ipv6
+from tasks.disable_ipv6_ufw import disable_ipv6_ufw
 from tasks.zsh import deploy_zsh
 from tasks.disable_ipv6 import disable_ipv6
 from tasks.shell_tools import deploy_shell_tools
 
 # server_upgrade()
 # deploy_server_user()
-# deploy_shell_tools()
+deploy_shell_tools()
 # deploy_zsh()
 # deploy_aliases()
 # deploy_ssh_keys()
@@ -20,4 +20,4 @@ from tasks.shell_tools import deploy_shell_tools
 # deploy_ssh_hardening()
 # deploy_root_password()
 # disable_ipv6()
-ufw_disable_ipv6()
+# ufw_disable_ipv6()
---
+++ xui/notes.md
@@ -0,0 +1,9 @@
+issue is that I cant take db from one server and run it on another.
+
+looks like it is needed to:
+- disable tg bot (conflicts)
+- disabl cert-related parts
+- ???
+
+>try recreate xui on same server with same configs
+>enable ports in ufw (why ios only? check protocols)
