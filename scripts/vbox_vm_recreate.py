511545a00e16392d5cfc3357fd28877dddbe77a9
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-03-23 03:58:33 +0500

    small fixes

--- scripts/vbox_vm_recreate.py
+++ scripts/vbox_vm_recreate.py
@@ -32,18 +32,18 @@ if vm_name in run("VBoxManage list vms"):
     print("VM deleted")
 
 print("Creating VM...")
-run("cloud-localds {seed_iso} {cloud_init}")
-run("VBoxManage import {ova_file} --vsys 0 --vmname {vm_name}")
-run("VBoxManage storageattach {vm_name} --storagectl IDE --port 1 --device 0 --type dvddrive --medium {seed_iso}")
-run("VBoxManage modifyvm {vm_name} --cpus {cpu_count} --memory {ram_in_mb}")
-run("VBoxManage startvm {vm_name} --type headless")
+run(f"cloud-localds {seed_iso} {cloud_init}")
+run(f"VBoxManage import {ova_file} --vsys 0 --vmname {vm_name}")
+run(f"VBoxManage storageattach {vm_name} --storagectl IDE --port 1 --device 0 --type dvddrive --medium {seed_iso}")
+run(f"VBoxManage modifyvm {vm_name} --cpus {cpu_count} --memory {ram_in_mb}")
+run(f"VBoxManage startvm {vm_name} --type headless")
 
 time.sleep(45)
 
 info = run(f"VBoxManage showvminfo {vm_name}")
 vm_state = info.split(':')[-1].strip()
 vm_os = info.split(':')[-2].strip()
-vm_ip = run(f"VBoxManage guestproperty get {vm_name} /VirtualBox/GuestInfo/Net/0/V4/IP").split()[-1]
+vm_ip = run(f"VBoxManage guestproperty get {vm_name} '/VirtualBox/GuestInfo/Net/0/V4/IP'").split()[-1]
 
 print("\n")
 print("VM started")
@@ -53,7 +53,7 @@ print(f"VM OS: {vm_os}")
 
 if not is_ip_valid(vm_ip):
     time.sleep(10)
-    vm_ip = run(f"VBoxManage guestproperty get {vm_name} /VirtualBox/GuestInfo/Net/0/V4/IP").split()[-1]
+    vm_ip = run(f"VBoxManage guestproperty get {vm_name} '/VirtualBox/GuestInfo/Net/0/V4/IP'").split()[-1]
 
     if not is_ip_valid(vm_ip):
         print("VM IP not found. Try it yourself")
