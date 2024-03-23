7cbe59622115e2554c2198bda03eac798e7bce8d
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-03-23 23:06:20 +0500

    resolving networking issues

--- scripts/vbox_vm_recreate.py
+++ scripts/vbox_vm_recreate.py
@@ -2,11 +2,9 @@
 
 import argparse
 import os
-import re
 import subprocess
 import time
 from pathlib import Path
-from typing import Callable
 
 from vm_postcreate import main as post_create
 
@@ -15,29 +13,23 @@ def _run(command: str) -> str:
     return subprocess.check_output(command, shell=True).decode().strip()
 
 
-def get_vm_ip(vm_name: str) -> str:
-    is_ip_valid: Callable = lambda ip_to_match: bool(re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip_to_match))
-    get_ip: Callable = lambda: _run(f"VBoxManage guestproperty get {vm_name} '/VirtualBox/GuestInfo/Net/0/V4/IP'").split()[-1]
-
-    ip = get_ip()
-    if not is_ip_valid(ip):
-        time.sleep(10)
-        ip = get_ip()
-
-    if not is_ip_valid(ip):
-        print("VM IP not found. Try it yourself")
-        exit(1)
-
-    return ip
+def _create_network_if_not_exists(network_name: str, network_ip: str):
+    if network_name not in _run("VBoxManage list hostonlyifs"):
+        _run("VBoxManage hostonlyif create")
+        _run(f"VBoxManage hostonlyif ipconfig {network_name} --ip {network_ip} --netmask 255.255.255.0")
 
 
 TMPDIR = os.getenv('TMPDIR', '/tmp')
 DEFAULT_OVA_FILE = "~/VMs/jammy-server-cloudimg-amd64.ova"
 DEFAULT_VM_NAME = "vbox-primary"
+DEFAULT_NETWORK_NAME = "vboxnet0"
+DEFAULT_NETWORK_IP = "192.168.56.1"
+DEFAULT_VM_IP = "192.168.56.101"
 DEFAULT_CPU_COUNT = 2
 DEFAULT_RAM_IN_MB = 2048
 
 
+# todo: fix display type (see vm settings)
 def recreate(
     ova_file_path: str = DEFAULT_OVA_FILE,
     vm_name: str = DEFAULT_VM_NAME,
@@ -46,7 +38,7 @@ def recreate(
 ):
     ova_file = Path(ova_file_path).expanduser()
     seed_iso = Path(TMPDIR) / "ubuntu-seed.iso"
-    cloud_init = "./vbox-vm-cloud-init.yml"
+    cloud_init = Path(__file__).parent / "vbox-vm-cloud-init.yml"
 
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
     print("Welcome to the VM starter script")
@@ -58,19 +50,23 @@ def recreate(
         _run(f"VBoxManage unregistervm {vm_name} --delete")
         print("VM deleted")
 
+    print("Ensuring network exists...")
+    _create_network_if_not_exists(DEFAULT_NETWORK_NAME, DEFAULT_NETWORK_IP)
+
     print("Creating VM...")
     _run(f"cloud-localds {seed_iso} {cloud_init}")
     _run(f"VBoxManage import {ova_file} --vsys 0 --vmname {vm_name}")
     _run(f"VBoxManage storageattach {vm_name} --storagectl IDE --port 1 --device 0 --type dvddrive --medium {seed_iso}")
     _run(f"VBoxManage modifyvm {vm_name} --cpus {cpu_count} --memory {ram_in_mb}")
+    _run(f"VBoxManage modifyvm {vm_name} --nic1 hostonly --hostonlyadapter1 {DEFAULT_NETWORK_NAME}")
     _run(f"VBoxManage startvm {vm_name} --type headless")
 
-    time.sleep(45)
+    time.sleep(30)
 
     info = _run(f"VBoxManage showvminfo {vm_name}")
     vm_state = info.split(':')[-1].strip()
     vm_os = info.split(':')[-2].strip()
-    vm_ip = get_vm_ip(vm_name)
+    vm_ip = DEFAULT_VM_IP  # todo: support hostonly ip assignment
 
     post_create(vm_name, vm_ip)
 
