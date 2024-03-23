4506e264ac74efef09d90fdaaeb8a255203266fe
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-03-23 14:11:17 +0500

    small changes in vbox_vm_recreate.py

--- scripts/vbox_vm_recreate.py
+++ scripts/vbox_vm_recreate.py
@@ -11,13 +11,13 @@ from typing import Callable
 from vm_postcreate import main as post_create
 
 
-def run(command: str) -> str:
+def _run(command: str) -> str:
     return subprocess.check_output(command, shell=True).decode().strip()
 
 
 def get_vm_ip(vm_name: str) -> str:
     is_ip_valid: Callable = lambda ip_to_match: bool(re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip_to_match))
-    get_ip: Callable = lambda: run(f"VBoxManage guestproperty get {vm_name} '/VirtualBox/GuestInfo/Net/0/V4/IP'").split()[-1]
+    get_ip: Callable = lambda: _run(f"VBoxManage guestproperty get {vm_name} '/VirtualBox/GuestInfo/Net/0/V4/IP'").split()[-1]
 
     ip = get_ip()
     if not is_ip_valid(ip):
@@ -43,7 +43,7 @@ def recreate(
     vm_name: str = DEFAULT_VM_NAME,
     cpu_count: int = DEFAULT_CPU_COUNT,
     ram_in_mb: int = DEFAULT_RAM_IN_MB
-) -> str:
+):
     ova_file = Path(ova_file_path).expanduser()
     seed_iso = Path(TMPDIR) / "ubuntu-seed.iso"
     cloud_init = "./vbox-vm-cloud-init.yml"
@@ -52,22 +52,22 @@ def recreate(
     print("Welcome to the VM starter script")
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-    if vm_name in run("VBoxManage list vms"):
+    if vm_name in _run("VBoxManage list vms"):
         print("VM already exists. Deleting it...")
-        run(f"VBoxManage controlvm {vm_name} poweroff")
-        run(f"VBoxManage unregistervm {vm_name} --delete")
+        _run(f"VBoxManage controlvm {vm_name} poweroff")
+        _run(f"VBoxManage unregistervm {vm_name} --delete")
         print("VM deleted")
 
     print("Creating VM...")
-    run(f"cloud-localds {seed_iso} {cloud_init}")
-    run(f"VBoxManage import {ova_file} --vsys 0 --vmname {vm_name}")
-    run(f"VBoxManage storageattach {vm_name} --storagectl IDE --port 1 --device 0 --type dvddrive --medium {seed_iso}")
-    run(f"VBoxManage modifyvm {vm_name} --cpus {cpu_count} --memory {ram_in_mb}")
-    run(f"VBoxManage startvm {vm_name} --type headless")
+    _run(f"cloud-localds {seed_iso} {cloud_init}")
+    _run(f"VBoxManage import {ova_file} --vsys 0 --vmname {vm_name}")
+    _run(f"VBoxManage storageattach {vm_name} --storagectl IDE --port 1 --device 0 --type dvddrive --medium {seed_iso}")
+    _run(f"VBoxManage modifyvm {vm_name} --cpus {cpu_count} --memory {ram_in_mb}")
+    _run(f"VBoxManage startvm {vm_name} --type headless")
 
     time.sleep(45)
 
-    info = run(f"VBoxManage showvminfo {vm_name}")
+    info = _run(f"VBoxManage showvminfo {vm_name}")
     vm_state = info.split(':')[-1].strip()
     vm_os = info.split(':')[-2].strip()
     vm_ip = get_vm_ip(vm_name)
