16bc5921787b9ddb5e4bf312214fe6008d643c9b
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-03-27 12:30:25 +0500

    almost done (bugs left)

--- os_setuper/apps_example.py
+++ os_setuper/apps_example.py
@@ -1,4 +1,4 @@
-import structured_config
+import remote_python
 from common import OS
 
 current_os = OS.ubuntu
@@ -18,13 +18,25 @@ def main():
     # ]
     # 
     # app.handle(apps)
-    structured_config.modify_config(
-        entries=[
-            structured_config.EntryState(Property="key", Value="value", Present=True, Overwrite=True),
-        ],
-        config_type=structured_config.ConfigType.json,
-        path="/var/lib/cloud/data/status.json",
-    )
+    # structured_config.modify_config(
+    #     entries=[
+    #         structured_config.EntryState(Property="key", Value="value", Present=True, Overwrite=True),
+    #     ],
+    #     config_type=structured_config.ConfigType.json,
+    #     path="/var/lib/cloud/data/status.json",
+    # )
+
+    def print_hello_world(x, y):
+        print(x, y)
+
+    remote_python.execute_from_function(func=print_hello_world, func_kwargs={"x": "Hello", "y": "World"})
+
+    def create_file_if_not_exists(file_path):
+        import os
+        if not os.path.exists(file_path):
+            open(file_path, "w").close()
+
+    remote_python.execute_from_function(func=create_file_if_not_exists, func_args=["/tmp/test.txt"])
 
 
 main()  # todo properly handle if __name__ == "__main__"
